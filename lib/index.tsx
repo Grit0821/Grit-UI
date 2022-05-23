d727003612cf6d82b4e3ad43bc9a924882cace99
wangyi <wangyi95821@gmail.com>
2022-05-23 21:40:12 +0800

    Icon 单元测试

--- lib/index.tsx
+++ lib/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Icon from './icon/icon'
 
 ReactDOM.render( <div>
-  <Icon name="amazon" onClick={() => console.log('an')}/>
+  <Icon name="amazon" onClick={() => console.log('')}/>
   <Icon name="apple"/>
   <Icon name="facebook"/>
   <Icon name="google"/>
